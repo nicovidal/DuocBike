da3e827a9b2185957758f11bcd6a8dadee19a155
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-05-03 19:14:06 -0400

    git pull

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,25 +5,25 @@ import { Admin } from "../bikeMainPage/pages/Admin";
 
 export const AppRouter = () => {
 
-    const authStatus='authenticatedAdmin';
+   /*  const authStatus='authenticatedAdmin'; */
 
   return (
     <Routes>
-        {
+{/*         {
             (authStatus==='authenticated')
             ? <Route path='/auth/*' element={<Login/>}/>
-            :<Route path='/*' element={<RegisterGuard/>}/>
+            :
         } 
         
         {
             (authStatus==='authenticatedAdmin')
-            ? <Route path='/admin/' element={<Admin/>}/>
-            :<Route path='/auth/*' element={<Login/>}/>
+            ? 
+            :
 
-        }
-
-        
-        
+        } */}
+<Route path='/*' element={<RegisterGuard/>}/>
+<Route path='/admin/' element={<Admin/>}/>
+<Route path='/auth/*' element={<Login/>}/>
         <Route path='/*' element={<Navigate to="/auth/login"/>}/>
     </Routes>
   )
