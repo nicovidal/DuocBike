fd0ecfece7d498392aa19dbff49f9b0dc979fc46
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-05-08 17:57:47 -0400

    tabla medianmante bonita

--- src/bikeMainPage/pages/BikeList.jsx
+++ src/bikeMainPage/pages/BikeList.jsx
@@ -5,34 +5,52 @@ import { useAlumnoStore } from "../../hooks";
 
 export const BikeList = () => {
 
-  const {alumno,startLoadingAlumno } =useAlumnoStore();
+  const { alumno, startLoadingAlumno } = useAlumnoStore();
 
 
   useEffect(() => {
-   
+
     startLoadingAlumno();
-   
+
   }, [])
-  
+
 
   return (
     <>
-    <Navbar/>
-  
-  
-      {alumno.map(a=>{
-        return(
-          <tr key={a.id}>
-            <td>{a.registerName}</td>
-            <td>{a.registerRut}</td>
-            <td>{a.registerCarrer}</td>
-            <td>{a.registerBrand}</td>
-            <td>{a.registerColor}</td>
-            <td>{a.registerID}</td>    
+      <Navbar />
+      <table className="table table-striped ">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Rut</th>
+            <th scope="col">Carrera</th>
+            <th scope="col">Marca</th>
+            <th scope="col">Color</th>
+
           </tr>
-        )
-      })}
-  
+        </thead>
+
+        {alumno.map(a => {
+          return (
+            <>
+              <tbody key={a.id}>
+                <tr>
+                  <td>{a.registerID}</td>
+                  <td>{a.registerName}</td>
+                  <td>{a.registerRut}</td>
+                  <td>{a.registerCarrer}</td>
+                  <td>{a.registerBrand}</td>
+                  <td>{a.registerColor}</td>
+                </tr>
+              </tbody>
+            </>
+
+          )
+
+        })}
+      </table>
+
     </>
   )
 }
