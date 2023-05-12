cb9ea6cf73c502aa57c8e7fc8682899686fd8396
Jaavispeed <ja.rodriguezc@duocuc.cl>
2023-05-11 21:21:39 -0400

    Avance css Lista Alumnos

--- src/bikeMainPage/pages/BikeList.jsx
+++ src/bikeMainPage/pages/BikeList.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "../components/Navbar"
 import { useAlumnoStore } from "../../hooks";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead,TableRow } from "@mui/material";
 
-
+import '../styles/BikeList.css'
 
 
 export const BikeList = () => {
@@ -55,37 +55,41 @@ export const BikeList = () => {
 
         })}
       </table> */}
-      <div className="container">
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">ID Bicicleta</TableCell>
-            <TableCell align="right">Nombre</TableCell>
-            <TableCell align="right">Rut</TableCell>
-            <TableCell align="right">Carrera</TableCell>
-            <TableCell align="right">Marca</TableCell>
-            <TableCell align="right">Color</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {alumno.map((a) => (
-            <TableRow
-              key={a.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="center">{a.registerID}</TableCell>
-              <TableCell align="right">{a.registerName}</TableCell>
-              <TableCell align="right">{a.registerRut}</TableCell>
-              <TableCell align="right">{a.registerCarrer}</TableCell>
-              <TableCell align="right">{a.registerBrand}</TableCell>
-              <TableCell align="right">{a.registerColor}</TableCell>
+      <div className="fondo4">
+        <div className="body4">
+          <div className="container4">
+            <TableContainer className="form4" component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table" >
+                <TableHead >
+                    <TableRow >
+                      <TableCell align="right">ID Bicicleta</TableCell>
+                      <TableCell align="right">Nombre</TableCell>
+                      <TableCell align="right">Rut</TableCell>
+                      <TableCell align="right">Carrera</TableCell>
+                      <TableCell align="right">Marca</TableCell>
+                      <TableCell align="right">Color</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                  {alumno.map((a) => (
+                    <TableRow
+                      key={a.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell align="center">{a.registerID}</TableCell>
+                      <TableCell align="right">{a.registerName}</TableCell>
+                      <TableCell align="right">{a.registerRut}</TableCell>
+                      <TableCell align="right">{a.registerCarrer}</TableCell>
+                      <TableCell align="right">{a.registerBrand}</TableCell>
+                      <TableCell align="right">{a.registerColor}</TableCell>
 
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+          </TableContainer>
+        </div>
+        </div>
     </div>
 
       
