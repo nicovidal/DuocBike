44a4f81fbb50f61cdfddcb3992fd2cd16719c67c
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-05-22 18:14:26 -0400

    little fix

--- src/adminMainPage/components/AddGuard.jsx
+++ src/adminMainPage/components/AddGuard.jsx
@@ -12,21 +12,34 @@ const guardFormFields = {
 export const AddGuard = () => {
 
     const { guardName, guardUser, guardPassword, onInputChange: onGuardInputChange } = useRegister(guardFormFields);
-    const { startRegisterGuard, errorMessage } = useAuthStore();
+    const { startRegisterGuard} = useAuthStore();
 
     const registerSubmit = (event) => {
         event.preventDefault();
-        startRegisterGuard({ guardName: guardName, guardUser: guardUser, guardPassword: guardPassword })
+
+        Swal.fire({
+            title: '¿Desea agregar a este guardia?',
+            icon: 'question',
+  /*           showDenyButton: true, */
+            showCancelButton: true, 
+            confirmButtonText: 'Agregar',
+     /*        denyButtonText: `No guardar`, */
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire('Agregado Correctamente', '', 'success')
+              startRegisterGuard({ guardName: guardName, 
+                guardUser: guardUser, 
+                guardPassword: guardPassword })
+
+  
+            } else if (result.isDenied) {
+              Swal.fire('No agregado', '', 'info')
+            }
+          })
+
     }
 
 
-    useEffect(() => {
-        if (errorMessage !== undefined) {
-            Swal.fire('Error en credenciales', errorMessage, 'error')
-        }
-
-
-    }, [errorMessage])
 
     return (
         <div className="container add-container">
