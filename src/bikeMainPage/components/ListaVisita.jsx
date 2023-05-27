0c438578d782c9445328e3fd2d3572b0429e6209
Nicolas Vidal <nico.vidal.m@gmail.com>
2023-05-26 23:18:38 -0400

    paginacion visitas

--- src/bikeMainPage/components/ListaVisita.jsx
+++ src/bikeMainPage/components/ListaVisita.jsx
@@ -1,15 +1,28 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import React, { useEffect } from 'react'
-import '../styles/ListaVisitas.css'
-import { useVisitaStore } from '../../hooks/useVisitaStore'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@mui/material';
+import { useEffect, useState } from 'react';
+import '../styles/ListaVisitas.css';
+import { useVisitaStore } from '../../hooks/useVisitaStore';
 
 export const ListaVisita = () => {
+  const { startLoadingVisitas, visita } = useVisitaStore();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const {startLoadingVisitas, visita} = useVisitaStore ()
   useEffect(() => {
     startLoadingVisitas();
-  })
-  
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const paginatedVisita = visita.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
   return (
     <>
       <div>
@@ -26,24 +39,31 @@ export const ListaVisita = () => {
                 <TableCell align="right">Salida</TableCell>
               </TableRow>
             </TableHead>
-              <TableBody>
-              {
-                visita.map((v) => (
-                  <TableRow key={v.id}>
-                    <TableCell align="right">{v.visitaRut}</TableCell>
-                    <TableCell align="right">{v.visitaNombre}</TableCell>
-                    <TableCell align="right">{v.visitaMarca}</TableCell>
-                    <TableCell align="right">{v.visitaLugar}</TableCell>
-                    <TableCell align="right">{v.visitaMotivo}</TableCell>
-                  </TableRow>
-                ))
-              }
-
-              </TableBody>
+            <TableBody>
+              {paginatedVisita.map((v) => (
+                <TableRow key={v.id}>
+                  <TableCell align="right">{v.visitaRut}</TableCell>
+                  <TableCell align="right">{v.visitaNombre}</TableCell>
+                  <TableCell align="right">{v.visitaMarca}</TableCell>
+                  <TableCell align="right">{v.visitaLugar}</TableCell>
+                  <TableCell align="right">{v.visitaMotivo}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
+          <TablePagination
+          component="div"
+          count={visita.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="Filas por página"
+          rowsPerPageOptions={[5, 10]}
+        />
         </TableContainer>
+
       </div>
     </>
-    
-  )
-}
+  );
+};
