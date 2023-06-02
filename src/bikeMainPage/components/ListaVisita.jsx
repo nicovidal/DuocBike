fb44ea6d73dc090eb3dafdbfbe3579878ead665d
Nicolas Vidal <nico.vidal.m@gmail.com>
2023-06-01 20:53:45 -0400

    ok visitas al registrar

--- src/bikeMainPage/components/ListaVisita.jsx
+++ src/bikeMainPage/components/ListaVisita.jsx
@@ -1,16 +1,21 @@
+
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@mui/material';
 import { useEffect, useState } from 'react';
-import '../styles/ListaVisitas.css';
 import { useVisitaStore } from '../../hooks/useVisitaStore';
 
 export const ListaVisita = () => {
   const { startLoadingVisitas, visita } = useVisitaStore();
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [visitaList, setVisitaList] = useState([]);
 
   useEffect(() => {
     startLoadingVisitas();
   }, []);
+
+  useEffect(() => {
+    setVisitaList(visita); 
+  }, [visita]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -21,39 +26,39 @@ export const ListaVisita = () => {
     setPage(0);
   };
 
-  const paginatedVisita = visita.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  const paginatedVisita = visitaList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
 
   return (
-    <>
-      <div>
-        <TableContainer component={Paper} className='listaVisita'>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">Rut</TableCell>
-                <TableCell align="right">Nombre</TableCell>
-                <TableCell align="right">Marca</TableCell>
-                <TableCell align="right">Lugar</TableCell>
-                <TableCell align="right">Motivo</TableCell>
-                <TableCell align="right">Entrada</TableCell>
-                <TableCell align="right">Salida</TableCell>
+    <div>
+      <TableContainer component={Paper} className='listaVisita'>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Rut</TableCell>
+              <TableCell align="right">Nombre</TableCell>
+              <TableCell align="right">Marca</TableCell>
+              <TableCell align="right">Lugar</TableCell>
+              <TableCell align="right">Motivo</TableCell>
+              <TableCell align="right">Entrada</TableCell>
+              <TableCell align="right">Salida</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedVisita.map((v) => (
+              <TableRow key={v.id}>
+                <TableCell align="right">{v.visitaRut}</TableCell>
+                <TableCell align="right">{v.visitaNombre}</TableCell>
+                <TableCell align="right">{v.visitaMarca}</TableCell>
+                <TableCell align="right">{v.visitaLugar}</TableCell>
+                <TableCell align="right">{v.visitaMotivo}</TableCell>
+                <TableCell align="right">{v.horaIngreso}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {paginatedVisita.map((v) => (
-                <TableRow key={v.id}>
-                  <TableCell align="right">{v.visitaRut}</TableCell>
-                  <TableCell align="right">{v.visitaNombre}</TableCell>
-                  <TableCell align="right">{v.visitaMarca}</TableCell>
-                  <TableCell align="right">{v.visitaLugar}</TableCell>
-                  <TableCell align="right">{v.visitaMotivo}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <TablePagination
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
           component="div"
-          count={visita.length}
+          count={visitaList.length} // Usa visitaList en lugar de visita
           page={page}
           onPageChange={handleChangePage}
           rowsPerPage={rowsPerPage}
@@ -61,9 +66,7 @@ export const ListaVisita = () => {
           labelRowsPerPage="Filas por página"
           rowsPerPageOptions={[5, 10]}
         />
-        </TableContainer>
-
-      </div>
-    </>
+      </TableContainer>
+    </div>
   );
 };
