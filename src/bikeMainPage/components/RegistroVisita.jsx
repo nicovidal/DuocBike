146bd64c72357982294f05ddeb43a72925025453
Nicolas Vidal <nico.vidal.m@gmail.com>
2023-06-01 20:53:40 -0400

    cargar visitas al regsitrar ok!

--- src/bikeMainPage/components/RegistroVisita.jsx
+++ src/bikeMainPage/components/RegistroVisita.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../styles/RegistroVisitas.css'
+// RegistroVisita.js
+import '../styles/RegistroVisitas.css';
 import { useVisitaStore } from '../../hooks/useVisitaStore';
 import Swal from 'sweetalert2';
 import { useRegister } from '../../hooks';
@@ -14,81 +14,104 @@ const visitasFormFields = {
 }
 
 export const RegistroVisita = () => {
-  const {visitaRut,visitaNombre,visitaMarca,visitaLugar,visitaMotivo, onInputChange: onRegisterInputChange } = useRegister(visitasFormFields);
-  const { startRegisterVisita } = useVisitaStore();
-  const registerVisitaSubmit = (event) => {
+  
+  const { visitaRut, visitaNombre, visitaMarca, visitaLugar, visitaMotivo, onInputChange: onRegisterInputChange } = useRegister(visitasFormFields);
+  const { startRegisterVisita, startLoadingVisitas } = useVisitaStore();
+
+  const registerVisitaSubmit = async (event) => {
     event.preventDefault();
 
     Swal.fire({
       title: '¿Desea agregar a esta visita?',
       icon: 'question',
-/*           showDenyButton: true, */
-      showCancelButton: true, 
+      showCancelButton: true,
       confirmButtonText: 'Agregar',
-/*        denyButtonText: `No guardar`, */
-    }).then((result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        Swal.fire('Agregado Correctamente', '', 'success')
-        startRegisterVisita({ 
-          visitaRut:visitaRut,
-          visitaNombre:visitaNombre,
-          visitaMarca:visitaMarca,
-          visitaLugar:visitaLugar,
-          visitaMotivo:visitaMotivo, })
+        Swal.fire('Agregado Correctamente', '', 'success');
+        await startRegisterVisita({
+          visitaRut: visitaRut,
+          visitaNombre: visitaNombre,
+          visitaMarca: visitaMarca,
+          visitaLugar: visitaLugar,
+          visitaMotivo: visitaMotivo,
+        });
 
+        startLoadingVisitas(); 
       } else if (result.isDenied) {
-        Swal.fire('No agregado', '', 'info')
+        Swal.fire('No agregado', '', 'info');
       }
-    })
-  }
+    });
+  };
 
   return (
-    
     <div className='fondoVisitas'>
       <div className='bodyVisitas'>
         <div className='containerVisita'>
           <h2 className="nuevaVisita">Registro visitas</h2><br />
 
-
           <form className='formVisitas' onSubmit={registerVisitaSubmit}>
             <div className='form-controlVisita'>
-              <input type="text" placeholder='Rut' className='inputVisitas' 
-              name="visitaRut" value={visitaRut}
-              onChange={onRegisterInputChange} required></input>
+              <input
+                type="text"
+                placeholder='Rut'
+                className='inputVisitas'
+                name="visitaRut"
+                value={visitaRut}
+                onChange={onRegisterInputChange}
+                required
+              />
               &nbsp;&nbsp;&nbsp;
 
-              <input type="text" placeholder='Nombre' className='inputVisitas' 
-              name="visitaNombre" value={visitaNombre }
-              onChange={onRegisterInputChange} required></input>
+              <input
+                type="text"
+                placeholder='Nombre'
+                className='inputVisitas'
+                name="visitaNombre"
+                value={visitaNombre}
+                onChange={onRegisterInputChange}
+                required
+              />
               &nbsp;&nbsp;&nbsp;
 
-              <input type="text" placeholder='Marca' className='inputVisitas' 
-              name="visitaMarca" value={visitaMarca}
-              onChange={onRegisterInputChange} required></input>
+              <input
+                type="text"
+                placeholder='Marca'
+                className='inputVisitas'
+                name="visitaMarca"
+                value={visitaMarca}
+                onChange={onRegisterInputChange}
+                required
+              />
               &nbsp;&nbsp;&nbsp;
-      
-              <input type="text" placeholder='Lugar' className='inputVisitas' 
-              name="visitaLugar" value={visitaLugar}
-              onChange={onRegisterInputChange} required></input>
+
+              <input
+                type="text"
+                placeholder='Lugar'
+                className='inputVisitas'
+                name="visitaLugar"
+                value={visitaLugar}
+                onChange={onRegisterInputChange}
+                required
+              />
               &nbsp;&nbsp;&nbsp;
-           
-              <input type="text" placeholder='Motivo' className='inputVisitas' 
-              name="visitaMotivo" value={visitaMotivo}
-              onChange={onRegisterInputChange} required></input>
-              
+
+              <input
+                type="text"
+                placeholder='Motivo'
+                className='inputVisitas'
+                name="visitaMotivo"
+                value={visitaMotivo}
+                onChange={onRegisterInputChange}
+                required
+              />
             </div>
             <button className='btn' value="Registrar">Registrar</button>
           </form>
 
-          <ListaVisita/>
+          <ListaVisita />
         </div>
-        
-
-        
       </div>
-      </div>
-
-  
-
-  )                   
-}
+    </div>
+  );
+};
