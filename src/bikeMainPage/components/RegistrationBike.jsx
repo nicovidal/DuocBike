9d5342de6534d7744011ba1068026e3acc63e1b3
Jaavispeed <ja.rodriguezc@duocuc.cl>
2023-06-15 18:07:25 -0400

    Validaciones mejores

--- src/bikeMainPage/components/RegistrationBike.jsx
+++ src/bikeMainPage/components/RegistrationBike.jsx
@@ -16,89 +16,104 @@ const registerFormFields = {
 
 
 export const RegistrationBike = () => {
-
-    
-    
     function enviarFormulario(){
         var nombre = document.getElementById('name')
-        
-        //para validar rut//
         var rut = document.getElementById('rut')
-        //para validar rut//
-
         var carrera = document.getElementById('carrera')
         var marca = document.getElementById('marca')
         var color = document.getElementById('color')
         var id = document.getElementById('id')
         var btn = document.getElementById('btn')
-        var error = document.getElementById('error')
         
-        var mensajesError = []
+
+        var error1 = document.getElementById('error1')
+        var error2 = document.getElementById('error2')
+        var error3 = document.getElementById('error3')
+        var error4 = document.getElementById('error4')
+        var error5 = document.getElementById('error5')
+        var error6 = document.getElementById('error6')
+
+        var mensajesError1 = []
+        var mensajesError2 = []
+        var mensajesError3 = []
+        var mensajesError4 = []
+        var mensajesError5 = []
+        var mensajesError6 = []
+
         var form = document.getElementById('form')
         let deshabilitar = false;
 
         /*VALIDACION NOMBRE*/
         if (nombre.value === null || nombre.value === '') {
-            mensajesError.push('Ingresa tu nombre. <br>');
+            mensajesError1.push('Ingresa tu nombre. <br>');
             deshabilitar = true;
         } else if (!/^[a-zA-Z]+$/.test(nombre.value)) {
-            mensajesError.push('El nombre solo debe contener letras. <br>');
+            mensajesError1.push('Solo debe contener letras. <br>');
             deshabilitar = true;
         } else if (nombre.value.length > 0 && nombre.value.length < 3) {
-            mensajesError.push('El nombre debe ser más largo. <br>');
+            mensajesError1.push('Debe ser más largo. <br>');
             deshabilitar = true;
         }
         /*VALIDACION NOMBRE*/
 
         /*VALIDACION RUT*/
         if (rut.value === null || rut.value === '') {
-            mensajesError.push('Ingresa un RUT. <br>');
+            mensajesError2.push('Ingresa un RUT. <br>');
             deshabilitar = true;
         } else if (!/^[0-9]+[0-9kK]$/.test(rut.value) || /-/.test(rut.value)) {
-            mensajesError.push('El RUT debe contener solo números y la "k" final, sin guiones. <br>');
+            mensajesError2.push('El RUT debe contener solo números y la "k" final, sin guiones. <br>');
             deshabilitar = true;
         } else if (rut.value.length < 9) {
-            mensajesError.push('El RUT debe tener al menos 9 dígitos. <br>');
+            mensajesError2.push('El RUT debe tener al menos 9 dígitos. <br>');
             deshabilitar = true;
         }
         /*VALIDACION RUT*/
 
         /*VALIDACION CARRERA*/
         if(carrera.value === null || carrera.value === ''){
-            mensajesError.push('Ingresa una carrera. <br>')
+            mensajesError3.push('Ingresa una carrera. <br>')
             deshabilitar=true;
         }
-        if(carrera.value.length >0 && carrera.value.length <3){
-            mensajesError.push('La carrera debe ser mas largo. <br>')
-            deshabilitar=true;
+        else if (!/^[a-zA-Z]+$/.test(carrera.value)) {
+            mensajesError3.push('Solo debe contener letras. <br>');
+            deshabilitar = true;
+        } else if (carrera.value.length > 0 && carrera.value.length < 3) {
+            mensajesError3.push('Debe ser más largo. <br>');
+            deshabilitar = true;
         }
         /*VALIDACION CARRERA*/
 
         /*VALIDACION MARCA*/
         if(marca.value === null || marca.value === ''){
-            mensajesError.push('Ingresa una marca. <br>')
+            mensajesError4.push('Ingresa una marca. <br>')
             deshabilitar=true;
         }
-        if(marca.value.length >0 && marca.value.length <3){
-            mensajesError.push('La marca debe ser mas largo. <br>')
-            deshabilitar=true;
+        else if (!/^[a-zA-Z]+$/.test(marca.value)) {
+            mensajesError4.push('Solo debe contener letras. <br>');
+            deshabilitar = true;
+        } else if (marca.value.length > 0 && marca.value.length < 3) {
+            mensajesError4.push('Debe ser más largo. <br>');
+            deshabilitar = true;
         }
         /*VALIDACION MARCA*/
 
         /*VALIDACION COLOR*/
         if(color.value === null || color.value === ''){
-            mensajesError.push('Ingresa un color. <br>')
+            mensajesError5.push('Ingresa un color. <br>')
             deshabilitar=true;
         }
-        if(color.value.length >0 && color.value.length <3){
-            mensajesError.push('El color debe ser mas largo. <br>')
-            deshabilitar=true;
+        else if (!/^[a-zA-Z]+$/.test(color.value)) {
+            mensajesError5.push('Solo debe contener letras. <br>');
+            deshabilitar = true;
+        } else if (color.value.length > 0 && color.value.length < 3) {
+            mensajesError5.push('Debe ser más largo. <br>');
+            deshabilitar = true;
         }
         /*VALIDACION COLOR*/
 
         /*VALIDACION ID*/
         if(id.value === null || id.value === ''){
-            mensajesError.push('Ingresa una ID. <br>')
+            mensajesError6.push('Ingresa una ID. <br>')
             deshabilitar=true;
         }
         if(id.value.length >0 && id.value.length <1){
@@ -106,14 +121,18 @@ export const RegistrationBike = () => {
             deshabilitar=true;
         }
         if(id.value <0){
-            mensajesError.push('Tiene que ser positivo. <br>')
+            mensajesError6.push('Tiene que ser positivo. <br>')
             deshabilitar=true;
         }
         /*VALIDACION ID*/
+        error1.innerHTML= mensajesError1.join('')
+        error2.innerHTML= mensajesError2.join('')
+        error3.innerHTML= mensajesError3.join('')
+        error4.innerHTML= mensajesError4.join('')
+        error5.innerHTML= mensajesError5.join('')
+        error6.innerHTML= mensajesError6.join('')
 
-        error.innerHTML= mensajesError.join('')
         form.addEventListener("keyup", enviarFormulario)
-
         if(deshabilitar === true){
             btn.disabled = true;
         }
@@ -121,16 +140,10 @@ export const RegistrationBike = () => {
             btn.disabled = false;
         }        
     }
-
-
-
     const { registerName, registerRut, registerCarrer, registerBrand, registerColor, registerID, onInputChange: onRegisterInputChange,onResetForm } = useRegister(registerFormFields);
-
     const { startRegisterAlumno } = useAuthStore();
-
     const registerSubmit = (event) => {
         event.preventDefault();
-                
         Swal.fire({
           title: '¿Desea agregar a este alumno?',
           icon: 'question',
@@ -154,9 +167,7 @@ export const RegistrationBike = () => {
             Swal.fire('No agregado', '', 'info')
           }
         })
-      
     }
-
     return (
         <div className='fondo3'>
             <div className='body3'>
@@ -170,7 +181,9 @@ export const RegistrationBike = () => {
                             name="registerName"
                             value={registerName}
                             onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error1' className='error'></div>
                         </div>
+                        
 
                         {/* Rut */}
                         <div className="form-control3">
@@ -178,7 +191,8 @@ export const RegistrationBike = () => {
                             <input type='text' placeholder='Rut: 123456785' className='input3' id='rut' maxLength={9}
                             name="registerRut"
                             value={registerRut}
-                            onChange={onRegisterInputChange} ></input>
+                            onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error2' className='error'></div>
                         </div>
 
                         {/* Carrera */}
@@ -188,6 +202,7 @@ export const RegistrationBike = () => {
                             name="registerCarrer"
                             value={registerCarrer}
                             onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error3' className='error'></div>
                         </div>
 
                         {/* Marca */}
@@ -197,6 +212,7 @@ export const RegistrationBike = () => {
                             name="registerBrand"
                             value={registerBrand}
                             onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error4' className='error'></div>
                         </div>
 
                         {/* Color */}
@@ -206,21 +222,21 @@ export const RegistrationBike = () => {
                             name="registerColor"
                             value={registerColor}
                             onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error5' className='error'></div>
                         </div>
 
                         {/* ID */}
                         <div className="form-control3">
-    
+
                             <input type='number' placeholder='ID' className='input3' id='id'
                             name="registerID"
                             value={registerID}
                             onChange={onRegisterInputChange} onClick={enviarFormulario}></input>
+                            <div id='error6' className='error'></div>
                         </div>
 
                         {/* Boton Registrar */}
                         <button className='btn' value="Registrar" id='btn' disabled>Registrar</button>
-                        <div id='error' className='error'></div>
-                        <div id="resultado"></div>
                     </form>
                     <img className="logo2" src="../assets/LogoDuoc.png"/>
                 </div>
